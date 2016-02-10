59775c3030e7bb8cb461da61a8d3e13ebf08f226
Mario Lukas <info@mariolukas.de>
2016-02-10 23:01:44 +0100

    fixed wifi setup script

--- delivery/018_wifi.sh
+++ delivery/018_wifi.sh
@@ -5,16 +5,16 @@ BannerEcho "wifi: Installing"
 
 AptInstall wpasupplicant || return 1
 
-"
+echo "
 
-auto wlan0
-allow-hotplug wlan0
-iface wlan0 inet dhcp
-wpa-conf /etc/wpa_supplicant/wpa_supplicant.conf
-iface default inet dhcp
+#auto wlan0
+#allow-hotplug wlan0
+#iface wlan0 inet dhcp
+#wpa-conf /etc/wpa_supplicant/wpa_supplicant.conf
+#iface default inet dhcp
 " >> /etc/network/interfaces
 
-"
+echo "
 update_config=1
 ctrl_interface=DIR=/var/run/wpa_supplicant GROUP=netdev
 eapol_version=1
@@ -30,7 +30,7 @@ auth_alg=OPEN
 " > /etc/wpa_supplicant/wpa_supplicant.conf
 
 # deactivate sleep mode
-"
+echo "
 options 8192cu rtw_power_mgnt=0 rtw_enusbss=0
 " > /etc/modprobe.d/8192cu.conf
 
