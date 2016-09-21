47187a55fd6bb54fb712a0600b8ff5fc26fe8631
Mario Lukas <info@mariolukas.de>
2016-09-21 22:22:44 +0200

    added custom 503 error page to haproxy

--- delivery/013_haproxy.sh
+++ delivery/013_haproxy.sh
@@ -32,4 +32,16 @@ frontend public
 backend fabscanpi
        server fabscanpi1 127.0.0.1:8080"  > /etc/haproxy/haproxy.cfg
 
+
+echo "HTTP/1.0 503 Service Unavailable^M
+Cache-Control: no-cache^M
+Connection: close^M
+Content-Type: text/html^M
+^M
+<html><body><h1>503 FabScanPi Server Unavailable</h1>
+Sometimes the FabScan Pi Server needs some seconds to start. Wait a few seconds and try again. <br>
+Otherwise check if the FabScanPi Server is running! 
+</body></html>
+" > /etc/haproxy/errors/503.http
+
 BannerEcho "Haproxy: Done"
