18897250d0b216b672e65a2d38aa3712e2341055
Mario Lukas <info@mariolukas.de>
2017-07-05 23:46:52 +0200

    fixed boot parameters

--- delivery/014_boot_options.sh
+++ delivery/014_boot_options.sh
@@ -15,16 +15,10 @@ echo "gpu_mem=128" >> /boot/config.txt
 # disable camera led
 echo "disable_camera_led=1" >> /boot/config.txt
 # max usb power
-echo "max_usb_current=1" >> /boot/config.txt
-# disabale bt on raspberry pi3
-echo "dtoverlay=pi3-miniuart-bt" >> /boot/config.txt
+#echo "max_usb_current=1" >> /boot/config.txt
 
-# disabale bt on raspberry pi3
-echo "enable_uart=1" >> /boot/config.txt
-
-# disabale bt on raspberry pi3
-echo "force_turbo=1" >> /boot/config.txt
-
+# disabale bt on raspberry pi3 to prevent serial port problems..
+echo "dtoverlay=pi3-disable-bt" >> /boot/config.txt
 
 
 echo "+dwc_otg.lpm_enable=0 console=tty1 root=/dev/mmcblk0p2 rootfstype=ext4 cgroup-enable=memory swapaccount=1 elevator=deadline rootwait" > /boot/cmdline.txt
