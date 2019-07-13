1e48c457014db862334a1dda7cbd445c3c44a62e
Mario Lukas <mariolukas@Marios-MBP.fritz.box>
2019-07-13 07:39:18 +0200

    o cleaned project

--- stage0/prerun.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash -e
-
-if [ ! -d "${ROOTFS_DIR}" ]; then
-	bootstrap stretch "${ROOTFS_DIR}" http://raspbian.raspberrypi.org/raspbian/
-fi
--- stage2/03-set-timezone/02-run.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash -e
-
-echo "Europe/London" > "${ROOTFS_DIR}/etc/timezone"
-rm "${ROOTFS_DIR}/etc/localtime"
-
-on_chroot << EOF
-dpkg-reconfigure -f noninteractive tzdata
-EOF
